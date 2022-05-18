3687e451ce106e1cdafede08be1d7da2b4326f40
lvg1610 <lvg1610@gmail.com>
2022-05-18 14:20:17 +0530

    First Commit v1

---
+++ SegueCalculator.java
@@ -0,0 +1,11 @@
+public class SegueCalculator implements Calculator
+{
+    public int sum(int x,int y)
+    {
+        return x+y;
+    }
+    public int square(int x)
+    {
+         return x*x;
+    }  
+}
